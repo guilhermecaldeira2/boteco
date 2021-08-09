ca763fe8d3bd59d8b9dd61b59cfae905fc200d36
Guilherme Silva <guilhermecaldeira.godoy@gmail.com>
2021-08-09 15:54:22 -0300

    add session module

---
+++ src/Session/index.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+import Cache from 'node-cache';
+
+import { Context } from '../Context';
+import { MiddlewareFn } from '../Middleware';
+
+export interface SessionContext<S extends Object> extends Context {
+  session?: S;
+}
+
+function getSessionKey(ctx: Context) {
+  return ctx.from.user.id;
+}
+
+export function Session<S extends Object>(): MiddlewareFn<SessionContext<S>> {
+  const store = new Cache({ stdTTL: 3600 });
+  const sessionKey = getSessionKey;
+  return async (ctx, next) => {
+    const key = sessionKey(ctx);
+    ctx.session = store.get(key);
+    if (!ctx.session) {
+      store.set(key, {});
+      ctx.session = {} as S;
+    }
+
+    await next();
+
+    if (!ctx.session) {
+      store.del(key);
+    } else {
+      store.set(key, ctx.session);
+    }
+  };
+}
+
+export default Session;
