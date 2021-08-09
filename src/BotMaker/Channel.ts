aa097aa493c091be2a92d0ff5045a927032b2b3b
Guilherme Silva <guilhermecaldeira.godoy@gmail.com>
2021-08-09 15:55:17 -0300

    add botmaker request maker

---
+++ src/BotMaker/Channel.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+import { AxiosRequestConfig } from 'axios';
+import { Request } from 'express';
+import { Update, Channel, SendMessageOptions } from '../Context';
+import MountBotMakerRequest from './MountRequest';
+import api from './api';
+
+class BotMakerChannel implements Channel {
+  public update: Update;
+
+  constructor(private readonly TOKEN: string, private readonly req: Request) {
+    this.update = MountBotMakerRequest(this.req);
+  }
+
+  sendMessage = async (text: string, options: SendMessageOptions) => {
+    return BotMakerChannel.sendMessage(text, this.TOKEN, options);
+  };
+
+  sendImage = async (photoUrl: string, options: SendMessageOptions) => {
+    return BotMakerChannel.sendImage(photoUrl, this.TOKEN, options);
+  };
+
+  static sendMessage = async (text: string, TOKEN: string, options: SendMessageOptions) => {
+    const config: AxiosRequestConfig = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'access-token': TOKEN,
+      },
+    };
+
+    const data = {
+      chatPlatform: options.chatPlatform,
+      chatChannelNumber: options.chatChannelNumber,
+      platformContactId: options.platformContactId,
+      messageText: text,
+    };
+
+    return api.post('/api/v1.0/message/v3', data, config);
+  };
+
+  static sendImage = async (photoUrl: string, TOKEN: string, options: SendMessageOptions) => {
+    const config: AxiosRequestConfig = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'access-token': TOKEN,
+      },
+    };
+
+    const data = {
+      chatPlatform: options.chatPlatform,
+      chatChannelNumber: options.chatChannelNumber,
+      platformContactId: options.platformContactId,
+      imageURL: photoUrl,
+    };
+
+    return api.post('/api/v1.0/message/v3', data, config);
+  };
+}
+
+export default BotMakerChannel;
